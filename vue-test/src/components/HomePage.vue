7a0a57cc9260aa789da8f1666bda41355b7cf6b7
JeffreyYangAndCelia <37099915+JeffreyYangAndCelia@users.noreply.github.com>
2018-08-16 01:28:59 +0800

    跨域

---
+++ vue-test/src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <p>Hello World! {{msg}}</p>
+    <button v-on:click="greet">点击</button>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      msg: 'Welcome To My HomePage!'
+    }
+  },
+  methods:{
+    greet(){
+      axios.get(this.HOST+'/demo/test').then(function(response){
+        alert(response);
+      }).catch(function(response){
+        alert(response);
+      })
+    }
+  }
+}
+</script>
